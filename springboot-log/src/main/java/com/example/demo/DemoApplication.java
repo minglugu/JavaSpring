3a372735d8b9f2e8b88b084804125adb0adb2bf4
Minglu Gu <gmlsyjsh@gmail.com>
2022-07-28 14:39:51 -0300

    add logging url from geeksforgeeks

--- springboot-log/src/main/java/com/example/demo/DemoApplication.java
+++ springboot-log/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+* logging in Spring Boot
+* https://www.baeldung.com/spring-boot-logging
+*
+*/
 @SpringBootApplication
 public class DemoApplication {
 
